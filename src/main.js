904a28613e0c2d0f9faa56a304fa381ce4416143
ElviaVega <elvita100@hotmail.com>
2019-04-16 22:36:47 -0500

    usando metodo Fetch y mostrando data en pantalla

--- src/main.js
+++ src/main.js
@@ -1,1 +1,30 @@
 /* Manejo del DOM */
+const containerElement = document.getElementById('conteiner');
+  function traer(){
+    fetch( 'https://raw.githubusercontent.com/Laboratoria/LIM009-DL-2.0/master/src/data/potter.json')
+      .then(res => res.json())
+      .then(data => mostrarData(data))
+        const mostrarData = (data) => {
+          let string = '';
+          for (let i = 0; i < data.length; i++) {
+            string += `<div class="card">
+                        <div>
+                          <img src=${data[i].image} alt=${data[i].name}  class='imagenRedonda'/>
+                        </div>  
+                        <div id='card' class='card1'>
+                          <h3> ${data[i].name} </h3>
+                          <h4>${data[i].house}</h4>
+                          <p>Especie: ${data[i].species}</p> 
+                                             
+                        </div> 
+                      </div>`
+           };
+          
+        return containerElement.innerHTML = string;
+      } 
+}
+traer();
+  
+
+
+
