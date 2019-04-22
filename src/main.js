6fee8adfee58e0a304152fdda46cdaa1b05e9a08
ElviaVega <elvita100@hotmail.com>
2019-04-22 09:06:06 -0500

    iniciando

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
 /* Manejo del DOM */
+
+/*
+let dataPotter = [];
+
+fetch('./data/potter.json')
+  .then(potter => potter.json())
+  .then(potter => {
+      dataPotter = potter;
+      console.log(potter)
+  });
+*/
+
+const cargarJson = () => {}
